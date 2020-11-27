7001d6326d54f5bd84d9de970e59dd5893ded8c6
tanjo <tanjo@users.noreply.github.com>
2020-11-27 13:40:27 +0900

    ATTrackingManager

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-# adia
+# adid
+
+## IDFA
+### iOS14.2 (Xcode Version 12.2 (12B45b))
+- `isAdvertisingTrackingEnabled` is changed by tracking setting.
+- `advertisingIdentifier` is returned `00000000-0000-0000-0000-000000000000` if tracking is turned off.
+- If it has been uninstalled, `ATTrackingManager.requestTrackingAuthorization` doesn't called, the past status will be reflected.
+- Completion handler of `ATTrackingManager.requestTrackingAuthorization` isn't **Main** thread. So, you need use `DispatchQueue.main.async` if you change UI.
+### iOS14.0-iOS14.1
+- `isAdvertisingTrackingEnabled` always returns false.
+
+## References
 
 - [AdSupport \| Apple Developer Documentation](https://developer.apple.com/documentation/adsupport)
 - [App Store Connect \- Support \- Apple Developer](https://developer.apple.com/support/app-store-connect/)
--- adid/Classes/ViewController.swift
+++ adid/Classes/ViewController.swift
@@ -5,19 +5,76 @@
 
 import UIKit
 import AdSupport
+import AppTrackingTransparency
 
 class ViewController: UIViewController {
 
   @IBOutlet weak var isAdvertisingTrackingEnabledResultLabel: CopiableLabel!
   @IBOutlet weak var advertisingIdentifierResultLabel: CopiableLabel!
+  @IBOutlet weak var systemVersionResultLabel: CopiableLabel!
+  @IBOutlet weak var trackingAuthorizationStatusResultLabel: CopiableLabel!
+  @IBOutlet weak var requestTrackingAuthorizationButton: UIButton!
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    if #available(iOS 14, *) {
+      self.requestTrackingAuthorizationButton.isEnabled = true
+    } else {
+      self.requestTrackingAuthorizationButton.isEnabled = false
+    }
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    self.refresh()
+  }
+  
+  // MARK: - Action methods
+  
+  @IBAction func didClickRequestTrackingAuthorization(_ sender: Any) {
+    if #available(iOS 14, *) {
+      ATTrackingManager.requestTrackingAuthorization(
+        completionHandler: self.showTrackingAuthorizationStatusResultLabel(state:))
+    }
+  }
+  
+  @IBAction func openSettings(_ sender: Any) {
+    guard let url = URL(string: UIApplication.openSettingsURLString) else {
+      return
+    }
+    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+  }
+  
+  // MARK: - Private methods
+  
+  @available(iOS 14, *)
+  func showTrackingAuthorizationStatusResultLabel(state: ATTrackingManager.AuthorizationStatus) -> Void {
+    DispatchQueue.main.async {
+      switch (state) {
+        case .notDetermined:
+          self.trackingAuthorizationStatusResultLabel.text = "notDetermined"
+        case .restricted:
+          self.trackingAuthorizationStatusResultLabel.text = "restricted"
+        case .denied:
+          self.trackingAuthorizationStatusResultLabel.text = "denied"
+        case .authorized:
+          self.trackingAuthorizationStatusResultLabel.text = "authorized"
+        @unknown default:
+          self.trackingAuthorizationStatusResultLabel.text = "unknown"
+      }
+    }
+  }
+  
+  func refresh() {
     isAdvertisingTrackingEnabledResultLabel.text =
       "\(ASIdentifierManager.shared().isAdvertisingTrackingEnabled)"
     advertisingIdentifierResultLabel.text =
       ASIdentifierManager.shared().advertisingIdentifier.uuidString
+    
+    systemVersionResultLabel.text = "\(UIDevice.current.systemVersion)"
+    
+    if #available(iOS 14, *) {
+      showTrackingAuthorizationStatusResultLabel(state: ATTrackingManager.trackingAuthorizationStatus)
+    }
   }
 }
 
